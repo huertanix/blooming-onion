2ef3b37fcd7b695fabf7337ef85afb2329d49502
David Huerta <huertanix@opentil.com>
2016-03-29 12:31:42 -0400

    Fixed relative path for config templates.

--- setup.sh
+++ setup.sh
@@ -10,7 +10,7 @@ apt-get update
 apt-get upgrade
 
 echo 'Installing Tor from official Tor software repository...'
-echo 'Type "y" and hit enter to install when asked.'
+echo 'Type "y" and hit enter to install Tor when asked.'
 add-apt-repository http://deb.torproject.org/torproject.org
 gpg --keyserver keys.gnupg.net --recv 886DDD89
 gpg --export A3C4F0F979CAA22CDBA8F512EE8CBC9E886DDD89 | apt-key add -
@@ -19,19 +19,19 @@ apt-get install tor deb.torproject.org-keyring
 
 echo 'Setting up Tor onion service...'
 mv /etc/tor/torrc /etc/tor/torrc.orig
-cp config/tor/torrc /etc/tor/.
+cp blooming-onion/config/tor/torrc /etc/tor/.
 service tor reload
 
 ONION_ADDRESS=`cat /var/lib/tor/hidden_service/hostname`
 
 echo 'Setting up the web server NGINX to use Tor onion service...'
 mv /etc/nginx/sites-available/ghost /etc/nginx/sites-available/ghost.orig
-cp config/nginx/ghost /etc/nginx/sites-available/.
+cp blooming-onion/config/nginx/ghost /etc/nginx/sites-available/.
 sed -i "s/ONION_PLACEHOLDER/$ONION_ADDRESS/g" /etc/nginx/sites-available/ghost
 
 echo 'Updating Ghost config to use .onion address...'
 mv /var/www/ghost/config.js /var/www/ghost/config.js.orig
-cp config/ghost/config.js /var/www/ghost/.
+cp blooming-onion/config/ghost/config.js /var/www/ghost/.
 sed -i "s/ONION_PLACEHOLDER/$ONION_ADDRESS/g" /var/www/ghost/config.js
 
 echo 'Restarting NGINX...'
